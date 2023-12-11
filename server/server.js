7cbbb4c03223ede7380ee0d8ff050cebc76d24b9
Augusto <augusporres@gmail.com>
2023-12-11 18:58:51 -0300

    borro carpetas no usadas

--- server/server.js
+++ server/server.js
@@ -37,7 +37,7 @@ app.use('/admin', require('./src/routes/adminRoutes'))
 app.all('*', (req, res) => {
     res.status(404)
     if (req.accepts('html')) {
-        res.sendFile(path.join(__dirname, '../client/pages/404.html'))
+        //res.sendFile(path.join(__dirname, '../client/pages/404.html'))
     } else if (req.accepts('json')) {
         res.json({ message: "404 Not Found"})
     } else {
